6515dd003148d168c9fbf0692494b7c2232793c0
MoizAsif <moiz.asif@xynotech.com>
2021-12-22 22:58:08 +0500

    Create Login Screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Input, Button } from 'react-native-elements';
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
-        <View>
-            <Text>Login Screen</Text>
+        <View style={styles.container}>
+            <Input
+                placeholder="Enter your email"
+                label="Email"
+                leftIcon={{ type: 'material', name: 'email' }}
+                value={email}
+                onChangeText={text => setEmail(text)}
+            />
+            <Input
+                placeholder="Enter your password"
+                label="Password"
+                leftIcon={{ type: 'material', name: 'lock' }}
+                value={password}
+                onChangeText={text => setPassword(text)}
+                secureTextEntry
+            />
+            <Button title="sign in" containerStyle={{width: 200, marginTop: 10}} />
+            <Button title="register" containerStyle={{width: 200, marginTop: 10}} />
         </View>
     )
 }
 
 export default LoginScreen
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 10
+    }
+})
