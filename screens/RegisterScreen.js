735b12d8c26f6832b2a216fe695193f3833dfccc
MoizAsif <moiz.asif@xynotech.com>
2021-12-22 23:17:01 +0500

    Add Register Screen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -2,11 +2,21 @@ import React, { useState } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { Input, Button } from 'react-native-elements';
 
-const LoginScreen = ({navigation}) => {
+const RegisterScreen = () => {
     const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
     const [password, setPassword] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+
     return (
         <View style={styles.container}>
+            <Input
+                placeholder="Enter your name"
+                label="Name"
+                leftIcon={{ type: 'material', name: 'badge' }}
+                value={name}
+                onChangeText={text => setName(text)}
+            />
             <Input
                 placeholder="Enter your email"
                 label="Email"
@@ -22,16 +32,20 @@ const LoginScreen = ({navigation}) => {
                 onChangeText={text => setPassword(text)}
                 secureTextEntry
             />
-            <Button title="sign in" containerStyle={{width: 200, marginTop: 10}} />
-            <Button 
-            title="register"
-            containerStyle={{width: 200, marginTop: 10}}
-            onPress={() => navigation.navigate('Register')} />
+            <Input
+                placeholder="Enter your image Url"
+                label="Profile Picture"
+                leftIcon={{ type: 'material', name: 'face' }}
+                value={imageUrl}
+                onChangeText={text => setImageUrl(text)}
+                secureTextEntry
+            />
+            <Button title="register" containerStyle={{width: 200, marginTop: 10}} />
         </View>
     )
 }
 
-export default LoginScreen
+export default RegisterScreen
 const styles = StyleSheet.create({
     container: {
         flex: 1,
